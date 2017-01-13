a2314c47221f039d8f8a42159293e35f7facfd5f
Alexander Borodin <aleksandrborodin1@MacBook-Pro-Aleksandr.local>
2017-01-13 21:32:47 +0300

    oauth production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ENV['OAUTH_REDIRECT'] = "http://localhost:3000"
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -249,9 +249,9 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, "187991601557688", "bdb81cb9c904c03885c8b28e28d1ced9",
-                  callback_url: "http://localhost:3000/users/auth/facebook/callback"
+                  callback_url: ENV['OAUTH_REDIRECT']+"users/auth/facebook/callback"
   config.omniauth :vkontakte, '5808367', '26wZk2NZB0fEuUHEl49y',
-                  callback_url: "http://localhost:3000/users/auth/vkontakte/callback"
+                  callback_url: ENV['OAUTH_REDIRECT']+"users/auth/vkontakte/callback"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
